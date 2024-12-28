9470a9cbea0b401edb61580d5428890d5e767321
Ritabrata Patra <teamritabrata@gmail.com>
2024-12-28 13:15:56 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+  <div>
+    <Image src="https://ik.imagekit.io/tzrgubobm/tr:w-300,h-300,fo-face:r-max,l-text,i-hello,fs-60,co-red,l-end/me2.png?updatedAt=1735370674771" alt="Picture of the author" width={500} height={500}/>
+  </div>
+  );
+}
