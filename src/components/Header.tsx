71937e7e86da2809c1346e2c9038aea1fd2c5867
Ritabrata Patra <teamritabrata@gmail.com>
2024-12-29 13:16:40 +0530

    changed bg layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ const Header = () => {
   return (
     <div className="border-b">
       <div className="flex h-16 items-center px-4">
-          <UserNav />
+        <UserNav />
         <MainNav className="mx-6" />
         <div className="ml-auto flex items-center space-x-4">
           <Search />
