3710d4aedcd6b703aac6d9508fb5c03394454b66
Ritabrata Patra <teamritabrata@gmail.com>
2024-12-31 10:19:24 +0530

    suspense around search abr to avoid deployement issues

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,19 +1,22 @@
 
 import { redirect } from "next/navigation";
 import SearchInput from "./SearchInput";
+import { Suspense } from "react";
 
 export function Search() {
   return (
+      <Suspense>
     <div>
-      <form action={
-        async(formData)=>{
+      <form
+        action={async (formData) => {
           "use server";
           const search = formData.get("search");
           redirect(`/search?q=${search}`);
-        }
-      }>
-        <SearchInput/>
+        }}
+      >
+          <SearchInput />
       </form>
     </div>
+        </Suspense>
   );
 }
