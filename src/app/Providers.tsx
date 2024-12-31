18c5031d3e5472c3a7499a3059c7dcfdea7793e3
Ritabrata Patra <teamritabrata@gmail.com>
2024-12-31 14:27:20 +0530

    created a loader for whenever api is called

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -1,10 +1,12 @@
 "use client";
 import { ImageKitProvider } from "imagekitio-next";
-import React from "react";
+import { Loader } from "lucide-react";
+import React, { useState } from "react";
 
 
 
 const authenticator = async () => {
+  
     try {
       const response = await fetch("/api/auth");
       console.log(response);
@@ -28,6 +30,28 @@ export const urlEndpoint = process.env.NEXT_PUBLIC_URL_ENDPOINT!;
 const publicKey = process.env.NEXT_PUBLIC_PUBLIC_KEY!;
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
+  const [loading, setLoading] = useState(true);
+  React.useEffect(() => {
+    // Simulate authenticator call to manage loading state
+    const initialize = async () => {
+      try {
+        await authenticator();
+      } catch (error) {
+        console.error("Error initializing authenticator:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    initialize();
+  }, []);
+  if (loading) {
+    return (
+      <div className="flex h-screen items-center justify-center bg-black">
+        <Loader className="animate-spin" size={50} color="blue" />
+      </div>
+    );
+  }
   
   return (
     <ImageKitProvider
