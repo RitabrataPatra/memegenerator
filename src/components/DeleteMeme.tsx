973dd268e35a2a4767061bbadf6e8bbc0a396eaf
Ritabrata Patra <teamritabrata@gmail.com>
2024-12-31 11:12:36 +0530

    improved the delete modal

--- src/components/DeleteMeme.tsx
+++ src/components/DeleteMeme.tsx
@@ -1,3 +1,4 @@
+"use client";   
 import React from "react";
 import {
   Dialog,
@@ -9,17 +10,24 @@ import {
 } from "@/components/ui/dialog";
 import { Trash2Icon } from "lucide-react";
 import { FileObject } from "imagekit/dist/libs/interfaces";
+import { Button } from "./ui/button";
 
 const DeleteMeme = ({file} : {file : FileObject}) => {
+    const handleDelete = () =>{
+        console.log("Delete succesfull!!");
+    }
   return (
     <Dialog>
       <DialogTrigger><Trash2Icon size={20} color="brown"/></DialogTrigger>
       <DialogContent>
         <DialogHeader>
-          <DialogTitle className="text-center"><span className="text-red-500">Delete</span> {file.customMetadata?.displayName ?? file.name} ?</DialogTitle>
-          <DialogDescription>
+          <DialogTitle className="text-center mb-2 line-clamp-1"><span className="text-red-500">Delete</span> {file.customMetadata?.displayName ?? file.name} ?</DialogTitle>
+          <DialogDescription className="flex flex-col gap-4">
             This action cannot be undone. This will permanently delete your
             file and remove it from our servers.
+            <Button variant="destructive" onClick={handleDelete}>
+            <p>Delete</p>
+            </Button>
           </DialogDescription>
         </DialogHeader>
       </DialogContent>
